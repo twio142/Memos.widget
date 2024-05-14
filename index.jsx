acea9a18fea27710910f2dcb82ad8f4badd84425
twio142 <64556708+twio142@users.noreply.github.com>
2024-05-14 16:14:26 +0200

    escape string in shell command

--- index.jsx
+++ index.jsx
@@ -139,7 +139,7 @@ const Resize = styled('div')`
 
 const readData = () => new Promise(resolve => run(`cat ${dataFile}`).then(data => resolve(JSON.parse(data || 'null'))));
 
-const writeData = (data) => run(`echo '${JSON.stringify(data).replaceAll("'", "'\\''")}' > ${dataFile}`);
+const writeData = (data) => run(`echo ${JSON.stringify(JSON.stringify(data))} > ${dataFile}`);
 
 const decreaseAllMemoIndexes = (key) =>
     document.querySelectorAll(`.memo:not([data-id="${key}"])`).forEach(m => m.style.zIndex = parseInt(m.style.zIndex) - 1 );
